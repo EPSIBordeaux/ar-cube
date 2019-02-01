cde17584e882db2a1834afeadb7f79b394412fc7
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-02-01 10:12:02 +0100

    Update 2019-02-01T09:11:59.823Z

--- js/custom.js
+++ js/custom.js
@@ -4,107 +4,109 @@ var renderer, camera, scene;
 
 var textureLoader = new THREE.TextureLoader();
 
-AFRAME.registerComponent('registerevents', {
-    init: function () {
-        this.marker = this.el;
-        var group = document.querySelector('a-box').object3D;
+function init() {
+    AFRAME.registerComponent('registerevents', {
+        init: function () {
+            this.marker = this.el;
+            var group = document.querySelector('a-box').object3D;
 
-        // MATERIALS 
-        var material_color_orange = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
-        var material_color_cyan = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
-        var material_color_lightOrange = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
-        var material_color_red = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
-        var material_color_violet = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+            // MATERIALS 
+            var material_color_orange = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+            var material_color_cyan = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+            var material_color_lightOrange = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+            var material_color_red = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+            var material_color_violet = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
 
-        // MESH 
-        square_bas = createAndPlaceSquare(material_color_orange, 0, -.5, 1);
-        square_bas.visible = false;
-        group.add(square_bas);
-        square_haut1 = createAndPlaceSquare(material_color_cyan, 0, -.5, -1);
-        square_haut1.visible = false;
-        group.add(square_haut1);
-        square_haut2 = createAndPlaceSquare(material_color_lightOrange, 0, -.5, -2);
-        square_haut2.visible = false;
-        group.add(square_haut2);
-        square_gauche = createAndPlaceSquare(material_color_red, -1, -.5, 0);
-        square_gauche.visible = false;
-        group.add(square_gauche);
-        square_droite = createAndPlaceSquare(material_color_violet, 1, -.5, 0);
-        square_droite.visible = false;
-        group.add(square_droite);
+            // MESH 
+            square_bas = createAndPlaceSquare(material_color_orange, 0, -.5, 1);
+            square_bas.visible = false;
+            group.add(square_bas);
+            square_haut1 = createAndPlaceSquare(material_color_cyan, 0, -.5, -1);
+            square_haut1.visible = false;
+            group.add(square_haut1);
+            square_haut2 = createAndPlaceSquare(material_color_lightOrange, 0, -.5, -2);
+            square_haut2.visible = false;
+            group.add(square_haut2);
+            square_gauche = createAndPlaceSquare(material_color_red, -1, -.5, 0);
+            square_gauche.visible = false;
+            group.add(square_gauche);
+            square_droite = createAndPlaceSquare(material_color_violet, 1, -.5, 0);
+            square_droite.visible = false;
+            group.add(square_droite);
 
-        cube_bas = createAndPlaceSquare(material_color_orange, 0, 0, 0.5);
-        cube_bas.rotateX(Math.PI / 2);
-        group.add(cube_bas);
-        cube_haut1 = createAndPlaceSquare(material_color_cyan, 0, 0, -.5);
-        cube_haut1.rotateX(Math.PI / 2);
-        group.add(cube_haut1);
-        cube_haut2 = createAndPlaceSquare(material_color_lightOrange, 0, 0.5, 0);
-        group.add(cube_haut2);
-        cube_gauche = createAndPlaceSquare(material_color_red, -0.5, 0, 0);
-        cube_gauche.rotateY(Math.PI / 2);
-        group.add(cube_gauche);
-        cube_droite = createAndPlaceSquare(material_color_violet, 0.5, 0, 0);
-        cube_droite.rotateY(Math.PI / 2);
-        group.add(cube_droite);
+            cube_bas = createAndPlaceSquare(material_color_orange, 0, 0, 0.5);
+            cube_bas.rotateX(Math.PI / 2);
+            group.add(cube_bas);
+            cube_haut1 = createAndPlaceSquare(material_color_cyan, 0, 0, -.5);
+            cube_haut1.rotateX(Math.PI / 2);
+            group.add(cube_haut1);
+            cube_haut2 = createAndPlaceSquare(material_color_lightOrange, 0, 0.5, 0);
+            group.add(cube_haut2);
+            cube_gauche = createAndPlaceSquare(material_color_red, -0.5, 0, 0);
+            cube_gauche.rotateY(Math.PI / 2);
+            group.add(cube_gauche);
+            cube_droite = createAndPlaceSquare(material_color_violet, 0.5, 0, 0);
+            cube_droite.rotateY(Math.PI / 2);
+            group.add(cube_droite);
 
-        this.marker.addEventListener('markerFound', function () {
-            var marker = this.object3D;
-            var markerId = marker.id;
-            console.log('markerFound', markerId);
+            this.marker.addEventListener('markerFound', function () {
+                var marker = this.object3D;
+                var markerId = marker.id;
+                console.log('markerFound', markerId);
 
-            var camera = document.querySelector('a-entity').object3D.children[0];
-            var video = document.querySelector('video');
+                var camera = document.querySelector('a-entity').object3D.children[0];
+                var video = document.querySelector('video');
 
-            var canvas = document.createElement('canvas');
-            canvas.width = video.offsetWidth;
-            canvas.height = video.offsetHeight;
+                var canvas = document.createElement('canvas');
+                canvas.width = video.offsetWidth;
+                canvas.height = video.offsetHeight;
 
-            var ctx = canvas.getContext('2d');
+                var ctx = canvas.getContext('2d');
 
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-            var texture = from(canvas.toDataURL());
+                var texture = from(canvas.toDataURL());
 
-            texture.get(getSquareCoordinates(camera, square_bas, video), function (url) {
-                loadTexture(cube_bas, url);
+                texture.get(getSquareCoordinates(camera, square_bas, video), function (url) {
+                    loadTexture(cube_bas, url);
+                });
+
+                texture.get(getSquareCoordinates(camera, square_droite, video), function (url) {
+                    loadTexture(cube_droite, url);
+                });
+
+                texture.get(getSquareCoordinates(camera, square_gauche, video), function (url) {
+                    loadTexture(cube_gauche, url);
+                });
+
+                texture.get(getSquareCoordinates(camera, square_haut1, video), function (url) {
+                    loadTexture(cube_haut1, url);
+                });
+
+                texture.get(getSquareCoordinates(camera, square_haut2, video), function (url) {
+                    loadTexture(cube_haut2, url);
+                });
+
             });
 
-            texture.get(getSquareCoordinates(camera, square_droite, video), function (url) {
-                loadTexture(cube_droite, url);
+            this.marker.addEventListener('markerLost', function () {
+                var marker = this.object3D;
+                var markerId = marker.id;
+                console.log('markerLost', markerId);
             });
+        }
+    });
 
-            texture.get(getSquareCoordinates(camera, square_gauche, video), function (url) {
-                loadTexture(cube_gauche, url);
-            });
+    AFRAME.registerComponent('setupscene', {
+        init: function () {
+            scene = this.el.object3D;
+            renderer = this.el.renderer;
+            camera = this.el.camera;
+        }
+    });
 
-            texture.get(getSquareCoordinates(camera, square_haut1, video), function (url) {
-                loadTexture(cube_haut1, url);
-            });
+}
 
-            texture.get(getSquareCoordinates(camera, square_haut2, video), function (url) {
-                loadTexture(cube_haut2, url);
-            });
-
-        });
-
-        this.marker.addEventListener('markerLost', function () {
-            var marker = this.object3D;
-            var markerId = marker.id;
-            console.log('markerLost', markerId);
-        });
-    }
-});
-
-AFRAME.registerComponent('setupscene', {
-    init: function () {
-        scene = this.el.object3D;
-        renderer = this.el.renderer;
-        camera = this.el.camera;
-    }
-});
-
-animate();
 
 function placeSquare(square_mesh, relativeX, relativeY, relativeZ) {
     if (relativeX != undefined) {
@@ -171,14 +173,4 @@ function loadTexture(element, texture) {
     })
 }
 
-function render() {
-    if (renderer != undefined) {
-        renderer.render(scene, camera);
-    }
-}
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    render();
-}
+init();
